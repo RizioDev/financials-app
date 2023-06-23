06091e7977808a6b8a872bae42929923865e88ae
RizioDev <fabrizioalderete23@gmail.com>
2023-06-23 01:35:52 -0300

    Creacion NavBar, LandingPage, Catalogo y Carousel

--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CarouselMarcas from "../Carousel/CarouselMarcas";
 
 const LandingPage = () => {
   return (
@@ -7,17 +8,18 @@ const LandingPage = () => {
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Payments tool for software companies
+              Compra o vende tu usado en tiempo récord.
             </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+              ¡Compra, vende y consigna autos usados y 0km sin complicaciones!
+              Expertos en asesoramiento personalizado, todos los bancos, solo
+              con DNI y sin intermediarios.
             </p>
             <a
               href="#"
               class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
-              Get started
+              Consignar
               <svg
                 class="w-5 h-5 ml-2 -mr-1"
                 fill="currentColor"
@@ -35,17 +37,18 @@ const LandingPage = () => {
               href="#"
               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-              Speak to Sales
+              Financia tu vehiculo
             </a>
           </div>
           <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
             <img
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+              src="https://plus.unsplash.com/premium_photo-1661274070000-d52df694d8d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
               alt="mockup"
             />
           </div>
         </div>
       </section>
+      <CarouselMarcas />
     </div>
   );
 };
